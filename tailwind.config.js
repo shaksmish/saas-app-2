88e3eb57196cc3b0a41fd538360279f9c0614403
Shalki Mishra <shaksmish@users.noreply.github.com>
2023-06-20 15:02:54 +0200

    adding core files

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+   './saas-app-2/index.html',
+  ],
+  theme: {
+    colors: {
+      "primary-01": "#49DEFF",
+      "primary-02": "#4FFFDF",
+      "primary-03": "#303030",
+      "primary-04": "#ffffff",
+      "secondary-01": "#4E4E4E",
+      "secondary-02": "#DEDEDE",
+      "secondary-03": "#F4F4F4",
+      "secondary-04": "#F9F9F9",
+    },
+    fontFamily: {
+      'jost': ['Jost', 'sans-serif'],
+      "open-sans": ["Open Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
